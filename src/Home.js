f12d3401a849695b74aeb62565593fb056e1d716
IamSahilvk <sahilkodgyale2000@gmail.com>
2022-03-17 12:41:18 +0530

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,177 @@
+import React from 'react';
+export default function Home()
+
+{
+return(
+    <body>
+    <nav>
+        <a href="">Cart</a>
+        <a href="">Notification</a>
+        <a id="icon" href="">Icon</a>
+    </nav>
+    <header>
+        <h1 id="Diana">
+            About Diana
+        </h1>
+        <h3>
+            A platform that looks out for you
+        </h3>
+        <p>
+            Diana empowers you to understand how your body works so you can look and feel your best.
+        </p>
+    </header>
+        <hr/>
+        <main>
+            <section class="First">
+            <h2 align="center">
+                Our Philosophy
+            </h2>
+            <h1>
+                Sustainable wellness is a big-picture, inside out approach
+            </h1>
+            <p>
+                <b> Diana is on a mission to create thoughtful, science-backed solutions for women who want to make informed decisions about their health and wellness.</b>
+            </p>
+            <p>
+                <b>That's why we've created an integrated ecosystem of: </b>
+            </p>
+            <img src="" alt="test tube"/>
+            <p>Consciously formulated products that deliver feel-good results fast </p>
+            <img src="" alt="stars"/>
+
+            <p>  In-app mood and period tracking options that help you cultivate a deeper connection with your mind and body </p>
+            <img src="" alt="phone"/>
+            
+            <p> 
+                In-app consultation portals that connect you with compassionate wellness experts
+            </p>
+            <p>
+                Consider us your nerdy ally who spends their time perfecting products that work because they want you to feel your best everyday.
+
+            </p>
+        </section>
+        <section>
+            <h2>What makes us different?</h2>
+            <h1>Reimagining and rewiring self-care</h1>
+            <p>
+                You care about conscious consumption, and so do we. 
+            </p> 
+            <p>
+
+
+            When it comes to your health, “good enough” is not good enough. We're here to flip the script on that narrative because you deserve the best 
+            </p>
+
+            <p> We collaborate with researchers and doctors to </p> 
+
+            <ul> <li> Help you get granular about your health and self-care with personalized insights, and </li></ul>
+
+            <ul><li>Equip you with simple, sustainable products that get the job done</li></ul>
+
+            <p> No to-the-moon-and-back claims in this neck of the woods.</p>
+
+            <p> 
+                We're just solving real problems for real people here. 
+             </p>
+
+        </section>
+        <section>
+            <h2>
+                Liberate your everyday wellness 
+            </h2>
+            <h1>
+                Shop our self-care products
+
+            </h1>
+            <p>
+                Gentle formulations thoughtfully engineered by specialist researchers and doctors to simplify your self-care and get results fast. And, we always list our ingredients - so what you see is what you get.   
+
+            </p>
+            <p class="button">
+                Upgrade your self-care
+            </p>
+
+            <p>#NoNasties, we promise!</p>
+            <img src="" alt="Flower"/>
+
+
+        </section>
+        <section>
+            <h1>Track your mood
+            </h1>
+            <p>
+                All days and all moods are not made equal. But we can only improve what we measure. Track your mood and vitals (including skin and hair health) so you can take better care of yourself every day. 
+
+            </p>
+            <p class="bbutton">Track you mood on the app</p>
+
+            <img src="" alt="img1"/>
+            <img src="" alt="img2"/>
+            <img src="" alt="img3"/>
+            <img src="" alt="img4"/>
+
+        
+        </section>
+        <section>
+            <h1>Track your period</h1>
+            <p>
+                Stay in the know with reliable AI-based and science-backed period, ovulation, and PMS predictions. The intuitive design makes tracking your cycle effortless and helps you stay on top of your monthly data. 
+ 
+
+            </p>
+            <p class="button">
+                Track you period on the app
+            </p>
+            <img src="" alt="Calendar"/>
+
+
+
+        </section>
+        <section>
+            <h1>Consult with wellness experts</h1>
+            <p>
+                Set up health consultations with experienced doctors and therapists on our app. Simply select a service, answer a few questions, and we’ll connect you to someone who is the right-fit for you. 
+
+
+            </p>
+            <p class="bbutton">
+                Get a consultation
+            </p>
+            <img src="" alt="Doctor"/>
+        </section>
+        <section>
+            <h1>Get your personalized period box
+            </h1>
+            <img src="" alt="medicine"/>
+            <p>
+                Tailor your monthly box of organic period products without the organic price tag (no pink taxing here). Get it delivered to your doorstep in sustainable packaging and track your subscription on our app. 
+            </p>
+            <p class="blackbutton">
+                Create your box on the app
+            </p>
+        </section>
+        <section>
+            <h1>With Diana, you're in control of your health, your every day, and your story.</h1>
+        </section>
+
+        </main>
+    
+    <footer>
+        
+        <a href=""><img src="" alt="twitter"/></a>
+        <a href="">Products</a>
+        <a href="">Our Science</a>
+        <a href="">Vision & Mission</a>
+        <a href="">About Us</a>
+        <hr/>
+        <form action="">
+            <p>Subscribe to our Newsletter</p>
+            <input type="email" value="Enter your email..." name="" id=""/>
+            <a href=""><img src="" alt="DM"/> Activate</a>
+        </form>
+    </footer>
+
+
+</body>
+)
+}
