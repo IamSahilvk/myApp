f43d62e6ce53a43b446158b7e682ec878a067e9b
IamSahilvk <sahilkodgyale2000@gmail.com>
2022-03-17 13:21:55 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './Home.js';
+import Different from './different.js';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-     
-     <Home/>  
+     <Home/>
+     <Different/>
     </div>
   );
 }
---
+++ src/different.js
@@ -0,0 +1,35 @@
+import React from "react";
+export default function Different() {
+  return (
+    <section>
+      <h2>What makes us different?</h2>
+      <h1>Reimagining and rewiring self-care</h1>
+      <p>You care about conscious consumption, and so do we.</p>
+      <p>
+        When it comes to your health, “good enough” is not good enough. We're
+        here to flip the script on that narrative because you deserve the best
+      </p>
+
+      <p> We collaborate with researchers and doctors to </p>
+
+      <ul>
+        {" "}
+        <li>
+          {" "}
+          Help you get granular about your health and self-care with
+          personalized insights, and{" "}
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          Equip you with simple, sustainable products that get the job done
+        </li>
+      </ul>
+
+      <p> No to-the-moon-and-back claims in this neck of the woods.</p>
+
+      <p>We're just solving real problems for real people here.</p>
+    </section>
+  );
+}
