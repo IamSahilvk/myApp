e35aed1d4477ae88e4cc4935422b4cfbd1b55fc8
IamSahilvk <sahilkodgyale2000@gmail.com>
2022-03-18 13:13:39 +0530

    Adding_consult_page

--- src/Consult.js
+++ src/Consult.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import About from './About.js';
-import Different from './different.js';
-import Third from './Third_section.js';
-import Track from './TrackMood_and_Period.js';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      
-     <About/>
-     <Different/>
-     <Third/>
-     <Track/>
-     <section class="sixth">
+export default function medicine()
+{
+    return(
+        <><section class="sixth">
             <br />
 
             <h1>Consult with wellness experts</h1>
@@ -51,12 +41,6 @@ function App() {
                                 story.
                             </h1>
                         </section>
-                        
-     
-     
-
-    </div>
-  );
-}
-
-export default App;
+                        </>
+                         )
+                        }
