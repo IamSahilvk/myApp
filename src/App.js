3485d014a677d9c40fce6e06875b9af0e4ee5eec
IamSahilvk <sahilkodgyale2000@gmail.com>
2022-03-18 12:12:39 +0530

    Third section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import About from './About.js';
 import Different from './different.js';
-
+import Third from './Third_section.js';
 import './App.css';
 
 function App() {
@@ -10,6 +10,8 @@ function App() {
       
      <About/>
      <Different/>
+     <Third/>
+
 
 
     </div>
---
+++ src/Third_section.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Third()
+{
+    return(
+        <><section class="third">
+            <h2><b> Liberate your everyday wellness</b> </h2>
+            <h1>Shop our self-care products</h1>
+            <div>
+                Gentle formulations thoughtfully engineered by specialist researchers
+                and doctors to simplify your self-care and get results fast. And, we
+                always list our ingredients - so what you see is what you get.
+            </div>
+            <div class="Button">
+                <p class="button">Upgrade your self-care</p>
+            </div>
+            <p>#NoNasties, we promise!</p>
+
+        </section><img src={require("./image/pexels-karolina-grabowska-4041391.jpg")} alt="flower" class="flower" /></>
+    )
+}
